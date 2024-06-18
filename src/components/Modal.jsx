5dc05a2435be58ceeae34aa935cc4de24518c04c
Mayank <mayank8328@gmail.com>
2024-06-18 18:27:44 +0530

    minor changes to modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ closeModal }) => {
     };
   }, []);
   return ReactDom.createPortal(
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-20">
       <div className="relative bg-white rounded-xl p-8 max-w-md w-full">
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
